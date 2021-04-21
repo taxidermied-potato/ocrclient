db601a8764b0e24142204358a08a0847cb9c367a
Al Yin <altyin@gmail.com>
2021-04-20 23:19:38 -0400

    Updates

---
+++ component---src-pages-404-js-fa5928406c568682f31c.js
@@ -0,0 +1,2 @@
+(self.webpackChunkocr_io=self.webpackChunkocr_io||[]).push([[883],{9616:function(e,t,n){"use strict";n.r(t);var u=n(7294),l=n(784),o=n(3751);t.default=function(){return u.createElement(l.Z,null,u.createElement(o.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-fa5928406c568682f31c.js.map
